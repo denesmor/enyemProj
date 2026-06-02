ad8dc52fba27a1ce1b7b1c6ed1b7ea8df50cfa99
denesmor <denesmorgotli@gmail.com>
2026-06-02 13:13:32 +0200

    Add project files.

---
+++ enyemProj/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace enyemProj
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void diakMenu_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            feladatPanel.Children.Clear();
+            feladatPanel.Children.Add(new UserControls.UserControlAuto());
+        }
+
+        private void tanarMenu_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            feladatPanel.Children.Clear();
+            feladatPanel.Children.Add(new UserControls.UserControlFelhasznalo());
+        }
+
+        private void osztalyMenu_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            feladatPanel.Children.Clear();
+            feladatPanel.Children.Add(new UserControls.UserControlVasarlo());
+        }
+
+        private void felhasznalokMenu_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            feladatPanel.Children.Clear();
+            feladatPanel.Children.Add(new UserControls.UserControlRolunk());
+        }
+
+        private void kilepesMenu_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
---
+++ enyemProj/UserControls/UserControlVasarlo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace enyemProj.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UserControlVasarlo.xaml
+    /// </summary>
+    public partial class UserControlVasarlo : Window
+    {
+        public UserControlVasarlo()
+        {
+            InitializeComponent();
+        }
+    }
+}
